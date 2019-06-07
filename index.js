c87a80280968ebef1b9361c785380a19cae48f2a
Katerina123456 <51322771+Katerina123456@users.noreply.github.com>
2019-06-07 16:01:13 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var express = require("express"),
-cors = require("cors");
+var express = require('express');
 var app = express();
-app.use(cors());
-var express = require("express");
-var app = express();
-app.get("/", function(request, response){
-    response.send("hello, Node.js!");
+ var server = require('http').createServer(app);
+ var io = require('socket.io').listen(server);
+ server.listen(591);
+ app.get('/', function(request, respons) {
+     respons.sendFile(___dirname + '/index.html');
 });
-app.listen(591);
-
-app.get("/folder/page", function(request, response) {
-    var x = request.query.foo;
-    response.send({ foo: x });
+connections = [];
+io.sockets.on('connection', function(socket) {
+    console.log("Успешное соединение!");
+    connections.push(socket);
+    socket.on("disconnect", function(data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Отключились!");
+    });
+    socket.on('send mess', function(data) {
+        io.sockets.emit('add mess', {mess: data.mess, name: data.name,
+        className: data.className});
+    });
 });
-
