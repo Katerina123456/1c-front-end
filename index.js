efb3349dd4b33d128eef817928a6392589f55c87
Katerina123456 <51322771+Katerina123456@users.noreply.github.com>
2019-06-07 16:06:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
+// Подключение всех модулей к программе
 var express = require('express');
 var app = express();
- var server = require('http').createServer(app);
- var io = require('socket.io').listen(server);
- server.listen(591);
- app.get('/', function(request, respons) {
-     respons.sendFile(___dirname + '/index.html');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+// Отслеживание порта
+server.listen(3000);
+
+// Отслеживание url адреса и отображение нужной HTML страницы
+app.get('/', function(request, respons) {
+	respons.sendFile(__dirname + '/index.html');
 });
+
+// Массив со всеми подключениями
 connections = [];
+
+// Функция, которая сработает при подключении к странице
+// Считается как новый пользователь
 io.sockets.on('connection', function(socket) {
-    console.log("Успешное соединение!");
-    connections.push(socket);
-    socket.on("disconnect", function(data) {
-        connections.splice(connections.indexOf(socket), 1);
-        console.log("Отключились!");
-    });
-    socket.on('send mess', function(data) {
-        io.sockets.emit('add mess', {mess: data.mess, name: data.name,
-        className: data.className});
-    });
+	console.log("Успешное соединение");
+	// Добавление нового соединения в массив
+	connections.push(socket);
+
+	// Функция, которая срабатывает при отключении от сервера
+	socket.on('disconnect', function(data) {
+		// Удаления пользователя из массива
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Отключились");
+	});
+
+	// Функция получающая сообщение от какого-либо пользователя
+	socket.on('send mess', function(data) {
+		// Внутри функции мы передаем событие 'add mess',
+		// которое будет вызвано у всех пользователей и у них добавиться новое сообщение 
+		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+	});
+
 });
+
